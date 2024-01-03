f48cc37721fd231ca9e86eb3f2483c12198839d7
Wesley de Paula <wesley.paula@mercadolivre.com.br>
2024-01-03 12:26:40 -0300

    first commit

---
+++ JogoDaForca/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JogoDaForca
+//
+//  Created by Wesley De Oliveira De Paula on 03/01/24.
+//
+
+#include <iostream>
+
+int maain(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
